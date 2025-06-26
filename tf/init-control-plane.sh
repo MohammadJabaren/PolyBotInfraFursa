791390087161145b60f51a0834c4cbe193b86c3f
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-06-26 20:39:23 +0300

    test the init file

--- tf/init-control-plane.sh
+++ tf/init-control-plane.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
+
 if [ ! -f /etc/kubernetes/admin.conf ]; then
-    sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+  sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 
-    mkdir -p \$HOME/.kube
-    sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
-    sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-    kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/calico.yaml
-  else
-    echo "✅ Cluster already initialized"
-  fi
-  #test
+  kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/calico.yaml
+else
+  echo "✅ Cluster already initialized"
+fi
